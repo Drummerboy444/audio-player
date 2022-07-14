b1db8850c32e362048c599c6feae2275ca8ecd8e
Anthony Murphy <anthonymurphy252@gmail.com>
2022-07-14 17:19:58 +0100

    Initial commit

---
+++ Audio Player/Assets/Code/Main.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    [SerializeField] private Track track;
+    
+    private void Start()
+    {
+        Debug.Log("Hello World!");
+    }
+}
